13b43f2eacd4587b636f5e425e7aa30e98ee41c5
gangasandhu <sr.gangasingh@gmail.com>
2024-03-13 14:27:53 -0700

    Nabar added to the home page. Stylesheets added and updated for dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './styles/App.css'
 import Home from './pages/Home'
+import './styles/darkly.min.css'
+import Header from './components/Header'
+
 
 function App() {
 
   return (
     <>
+      <Header />
       <Home />
     </>
   )
