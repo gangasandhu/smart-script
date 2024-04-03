c22c264e05e1595832ba00c796e9533de1eacd40
gangasandhu <sr.gangasingh@gmail.com>
2024-04-03 01:24:01 -0700

    Add: smartscript logo to website

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from "../assets/SmartScript-logo.png"
 
-const Header = ({theme}) => {
+const Header = ({ theme }) => {
     return (
         <nav className={`navbar navbar-expand-lg ${theme == 'light' ? 'bg-white' : 'bg-dark'}`} data-bs-theme={theme == 'light' ? "light" : "dark"}>
             <div className="container-fluid">
-                <Link className="navbar-brand text-success" to="smart-script/">SmartScript</Link>
+                <Link className="navbar-brand text-success" to="smart-script/">
+                    <img className='me-2' width={40} src={logo} alt="SmartScript Logo" />
+                    SmartScript
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -25,9 +29,9 @@ const Header = ({theme}) => {
                         <li className="nav-item">
                             <Link className="nav-link" to="smart-script/team">Team</Link>
                         </li>
-                       
+
                     </ul>
-          
+
                 </div>
             </div>
         </nav>
